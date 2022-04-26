052281f0834b488ece4915a78ae74677062c6d65
Mamta1994 <37013670+Mamta1994@users.noreply.github.com>
2022-04-26 13:48:29 +0530

    Update client.py

--- tap_brightview/client.py
+++ tap_brightview/client.py
@@ -2,17 +2,13 @@ import jaydebeapi
 import jpype
 import tap_brightview.helpers as helper
 import singer
-
 LOGGER = singer.get_logger()
-
-
 class HiveClient:
     def __init__(self, config):
         self.BV_USER = config["BV_USER"]
         self.BV_PASSWORD = config["BV_PASSWORD"]
         self.client = self.setup_connection()
         self.sql = self.client.cursor()
-
     def setup_connection(self):
         connection = jaydebeapi.connect(
             "com.simba.hive.jdbc.HS2Driver",
@@ -21,7 +17,6 @@ class HiveClient:
             "./HiveJDBC42.jar",
         )
         return connection
-
     def query_database(
         self,
         schema,
@@ -32,34 +27,36 @@ class HiveClient:
         limit_key="last_operation_time",
         limit_key_value="1970-01-11 00:00:00.000000",
     ):
-
         row_count = 0
         order_by = f"ORDER BY {limit_key}, {id} "
-
         if id == None:
             order_by = f"ORDER BY {limit_key} "
-        
+
         if table == "procedure":
             table = "`procedure`"
-
+        
         LOGGER.info("Querying DB")
-        self.sql.execute(
-            "SELECT * "
-            + f"FROM {table} "
-            + f'WHERE {limit_key} >= "{limit_key_value}" '
-            + order_by
-            + f"LIMIT {limit} OFFSET {offset}"
-        )
-
+        if table == "mv_impact_data_response":
+            query = "SELECT * "
+                + "FROM"
+                + "(select *,"
+                + "case when last_operation_time is null then created_date"
+                + "else last_operation_time"
+                + "end as incremental_key"
+                + f"from {table} ) sub_query"
+                + f'WHERE incremental_key >= "{limit_key_value}" '  
+                + order_by 
+                + f"LIMIT {limit} OFFSET {offset}"
+        else:
+            query = "SELECT * "
+                + f"FROM {table} "
+                + f'WHERE {limit_key} >= "{limit_key_value}" '
+                + order_by
+                + f"LIMIT {limit} OFFSET {offset}"
+        LOGGER.info("Running query ", query)
+        self.sql.execute(query)
         LOGGER.info("Query Complete.  Starting rows")
         row = ""
 
         while row is not None:
             row_count += 1
-            row = self.sql.fetchone()
-            if row == None:
-                self.sql._close_last()
-                return row
-            if row_count % 10000 == 0:
-                LOGGER.info(f"Row count = {row_count}")
-            yield helper.create_json_response(schema, row)
