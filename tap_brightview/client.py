8a6a38709a6ec555f082e881430338150bf6d58d
Mamta1994 <37013670+Mamta1994@users.noreply.github.com>
2022-04-27 23:52:11 +0530

    Update client.py

--- tap_brightview/client.py
+++ tap_brightview/client.py
@@ -36,11 +36,19 @@ class HiveClient:
         LOGGER.info("Querying DB")
         if table == "mv_impact_data_response":
             LOGGER.info("Executing query")
+            
+            LOGGER.info("SELECT * FROM "
+            + "(select *,case when last_operation_time is null then created_datetime else last_operation_time end as incremental_key from mv_impact_data_response ) sub_query "
+            + f'WHERE incremental_key >= "{limit_key_value}" '
+            + order_by
+            + f"LIMIT {limit} OFFSET {offset}")
+            
             self.sql.execute("SELECT * FROM "
             + "(select *,case when last_operation_time is null then created_datetime else last_operation_time end as incremental_key from mv_impact_data_response ) sub_query "
             + f'WHERE incremental_key >= "{limit_key_value}" '
             + order_by
             + f"LIMIT {limit} OFFSET {offset}")
+            
             LOGGER.info("End of the query")
         else:
             self.sql.execute("SELECT * "
